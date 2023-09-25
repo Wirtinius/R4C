6b0796d7c84df306af196f5e6cd417e4a981e51f
Wirtinius <115718269+Wirtinius@users.noreply.github.com>
2023-09-25 14:00:23 +0600

    Start of the project

---
+++ robots/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Robot(models.Model):
+    serial = models.CharField(max_length=5, blank=False, null=False)
+    model = models.CharField(max_length=2, blank=False, null=False)
+    version = models.CharField(max_length=2, blank=False, null=False)
+    created = models.DateTimeField(blank=False, null=False)
